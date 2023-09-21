173c04f6cdf5402c9c9a6146c2bcf8dc502209ac
JohnElnaugh <john.elnaugh@bytes.co.uk>
2023-09-21 16:31:31 +0100

    refactor: optimise code

--- main.go
+++ main.go
@@ -9,79 +9,80 @@ import (
 	"time"
 )
 
-// Profile holds the status text and emoji
 type Profile struct {
 	StatusText  string `json:"status_text"`
 	StatusEmoji string `json:"status_emoji"`
 }
 
-// Payload wraps the profile data for API request
 type Payload struct {
 	Profile Profile `json:"profile"`
 }
 
-func DaysUntilChristmas() int {
-	// Get the current time
-	currentTime := time.Now()
-
-	// Create a new time object for December 25th of the current year
-	christmas := time.Date(currentTime.Year(), time.December, 25, 0, 0, 0, 0, currentTime.Location())
-
-	// Calculate the duration between now and Christmas
-	duration := christmas.Sub(currentTime)
-
-	// Convert duration to days
-	days := int(duration.Hours() / 24)
-
-	return days
+type SlackClient struct {
+	httpClient *http.Client
+	token      string
 }
 
-func main() {
-	// Replace this with your Bot User OAuth Token
-	token := os.Getenv("AUTH_TOKEN")
+func NewSlackClient(token string) *SlackClient {
+	return &SlackClient{
+		httpClient: &http.Client{},
+		token:      token,
+	}
+}
 
-	client := &http.Client{}
+func (sc *SlackClient) updateStatus(profile Profile) error {
 	url := "https://slack.com/api/users.profile.set"
-
-	daysUntilChristmas := DaysUntilChristmas()
-
-	statusTextString := fmt.Sprintf("%d days until Christmas", daysUntilChristmas)
-
-	// Create payload
-	profile := Profile{StatusText: statusTextString, StatusEmoji: ":christmas_tree:"}
-	
 	payload := Payload{Profile: profile}
-
-	// Convert payload to JSON
 	jsonPayload, err := json.Marshal(payload)
 	if err != nil {
-		fmt.Println("Error marshaling payload:", err)
-		return
+		return fmt.Errorf("error marshaling payload: %w", err)
 	}
 
-	// Create a new request
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonPayload))
 	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return
+		return fmt.Errorf("error creating request: %w", err)
 	}
 
-	// Set headers
-	req.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("Authorization", "Bearer "+sc.token)
 	req.Header.Set("Content-Type", "application/json")
 
-	// Make the API call
-	resp, err := client.Do(req)
+	resp, err := sc.httpClient.Do(req)
 	if err != nil {
-		fmt.Println("Error making request:", err)
-		return
+		return fmt.Errorf("error making request: %w", err)
 	}
 	defer resp.Body.Close()
 
-	// Check for successful response
-	if resp.StatusCode == http.StatusOK {
-		fmt.Println("Status updated successfully.", resp.Body)
-	} else {
-		fmt.Printf("Failed to update status. HTTP Status Code: %d\n", resp.StatusCode)
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to update status. HTTP Status Code: %d", resp.StatusCode)
 	}
+
+	return nil
+}
+
+func daysUntilChristmas() int {
+	currentTime := time.Now()
+	christmas := time.Date(currentTime.Year(), time.December, 25, 0, 0, 0, 0, currentTime.Location())
+	duration := christmas.Sub(currentTime)
+	return int(duration.Hours() / 24)
+}
+
+func main() {
+	token := os.Getenv("AUTH_TOKEN")
+	if token == "" {
+		fmt.Println("error: AUTH_TOKEN environment variable is not set.")
+		return
+	}
+
+	sc := NewSlackClient(token)
+
+	days := daysUntilChristmas()
+	statusText := fmt.Sprintf("%d days until Christmas", days)
+	profile := Profile{StatusText: statusText, StatusEmoji: ":christmas_tree:"}
+
+	if err := sc.updateStatus(profile); err != nil {
+		fmt.Println("Failed to update Slack status:", err)
+		return
+	}
+
+	fmt.Println("Status updated successfully.")
 }
